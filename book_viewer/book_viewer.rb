3e28f299f0132d531a2ec5663fc5f39fea524cbd
Alexandra Prodan <prodan.sasha@gmail.com>
2016-08-15 16:44:50 -0500

    Book viewer initial commit

---
+++ book_viewer/book_viewer.rb
@@ -0,0 +1,10 @@
+require "tilt/erubis"
+require "sinatra"
+require "sinatra/reloader"
+
+get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+  @contents = File.readlines("data/toc.txt")
+
+  erb :home
+end
