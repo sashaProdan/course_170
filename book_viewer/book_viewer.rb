ffa12d1bb5a3770da6c04842b38f045db15a2282
Alexandra Prodan <prodan.sasha@gmail.com>
2016-08-16 14:36:17 -0500

    Add helpers for chapter text proper indentation

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -2,20 +2,28 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").join("<p></p>")
+  end
+end
+
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
 
   erb :home
 end
 
 get "/chapters/:number" do
-  @contents = File.readlines("data/toc.txt")
-  number = params[:number]
-  chapter_name = @contents[number.to_i - 1]
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
   @title = "Chapter #{number}: #{chapter_name}"
+
   @chapter = File.read("data/chp#{number}.txt")
-  
+
   erb :chapter
 end
-
