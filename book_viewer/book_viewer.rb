3a08511f0764d8fa2ceff83bea94ff31140ec219
Alexandra Prodan <prodan.sasha@gmail.com>
2016-08-16 17:32:27 -0500

    Add search form

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -4,7 +4,13 @@ require "sinatra/reloader"
 
 helpers do
   def in_paragraphs(text)
-    text.split("\n\n").join("<p></p>")
+    text.split("\n\n").each_with_index.map do |line, index|
+      "<p id=paragraph#{index}>#{line}</p>"
+    end.join
+  end
+
+  def highlight(text, term)
+    text.gsub(term, "<strong>#{term}</strong>")
   end
 end
 
@@ -26,4 +32,39 @@ get "/chapters/:number" do
   @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
+end
+
+def each_chapter(&block)
+  @contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+# This method returns an Array of Hashes representing chapters that match the
+# specified query. Each Hash contain values for its :name and :number keys.
+def chapters_matching(query)
+  results = []
+
+  return results unless query
+
+  each_chapter do |number, name, contents|
+    matches = {}
+    contents.split("\n\n").each_with_index do |paragraph, index|
+      matches[index] = paragraph if paragraph.include?(query)
+    end
+    results << {number: number, name: name, paragraphs: matches} if matches.any?
+  end
+
+  results
+end
+
+get "/search" do
+  @results = chapters_matching(params[:query])
+  erb :search
+end
+
+not_found do
+  redirect "/"
 end
